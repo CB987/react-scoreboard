70b3a6d9c9dcbc5662efd5a891dbf665b0795242
Clare Barton <grimalkinclare@gmail.com>
2018-12-03 11:43:49 -0500

    defines 2 versions of icrementScoreById fxn

--- src/App.js
+++ src/App.js
@@ -41,11 +41,50 @@ class App extends Component {
       return (
         <div key={score.id}>
           <h2>name: {score.name} // score: {score.score}</h2>
+          <button onClick={() => this._incrementScoreById(score.id)}>+</button>
         </div>
       );
     });
     return cards
   }
+//version 1 way to do it: .map, manually comstructing replacement
+  // _incrementScoreById(id) {
+  //   // find the player in this.state.scores
+  //   //increment their score
+  //   const newScores = this.state.scores.map(jeff => {
+  //     if (jeff.id !== id) {
+  //       return jeff
+  //     } else {
+  //       return {
+  //         id: jeff.id,
+  //         name : jeff.name,
+  //         score: jeff.score + 1
+  //       }
+  //     }
+  //   })
+  //   
+//version 2: .map, use shorthand to copy values out of the original
+_incrementScoreById(id) {
+  //find the player in this.state.scores
+  //increment their score
+  const newScores = this.state.scores.map(bruce => {
+    if (bruce.id !== id) {
+      return bruce;
+    } else {
+      return {
+        ...bruce, 
+        score: bruce.score + 1
+      };
+    }
+  });
+  // and call this.setState to rerender 
+    this.setState({
+      scores: newScores
+    })
+    console.log('hellooooo');
+  }
 }
+
+
 
 export default App;
