5ec000b44ed33498b7464c091cb0e57d2452ea2d
Clare Barton <grimalkinclare@gmail.com>
2018-12-03 10:27:20 -0500

    sets state and renders h1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    //Set up state
+    this.state = {
+      scores: [
+        {
+          id: 1,
+          name: "ahjuma",
+          score: 1001
+        }, {
+          id: 2,
+          name: "jeffles",
+          score: 20
+        }, {
+          id: 3,
+          name: "stinkface",
+          score: 500
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> scoreboard app</h1>
+        <div className="score-container">
+          {this._scoresAsCards()}
+        </div>
       </div>
     );
   }
